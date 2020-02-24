f2f5419c4caafed48657001cf8ca8bee009ac262
duyuqian <15738381799@163.com>
2020-02-24 22:35:20 +0800

    [duyuqian] feat: complete the copyDirectory method

--- io/src/main/java/com/thoughtworks/io/FileUtil.java
+++ io/src/main/java/com/thoughtworks/io/FileUtil.java
@@ -1,18 +1,52 @@
 package com.thoughtworks.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class FileUtil {
-
-    /**
-     * 完成复制文件夹方法:
-     * 1. 把给定文件夹from下的所有文件(包括子文件夹)复制到to文件夹下
-     * 2. 保证to文件夹为空文件夹，如果to文件夹不存在则自动创建
-     * <p>
-     * 例如把a文件夹(a文件夹下有1.txt和一个空文件夹c)复制到b文件夹，复制完成以后b文件夹下也有一个1.txt和空文件夹c
-     */
     public static void copyDirectory(File from, File to) throws IOException {
+        if (!to.exists()) {
+            System.out.println("新建文件：" + to.mkdirs());
+        } else {
+            deleteAllFile(to);
+        }
+        File[] fromList = from.listFiles();
+        if (fromList != null) {
+            for (File file : fromList) {
+                File fromFile = new File(from.getPath() + File.separator + file.getName());
+                File toFile = new File(to.getPath() + File.separator + file.getName());
+                if (file.isDirectory()) {
+                    copyDirectory(fromFile, toFile);
+                } else {
+                    copyFile(fromFile, toFile);
+                }
+            }
+        }
+    }
 
+    public static void deleteAllFile(File file) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            assert files != null;
+            for (File f : files) {
+                deleteAllFile(f);
+            }
+        } else {
+            System.out.println("删除文件：" + file.delete());
+        }
+    }
+
+    public static void copyFile(File from, File to) throws IOException {
+        try (InputStream input = new FileInputStream(from);
+             OutputStream output = new FileOutputStream(to)) {
+            int n;
+            while ((n = input.read()) != -1) {
+                output.write(n);
+            }
+        }
     }
 }
